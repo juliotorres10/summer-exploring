987d3a0ddbb00c478fd678a32e2b2086a35309cf
juliotorres10 <111753831+juliotorres10@users.noreply.github.com>
2022-09-14 11:55:54 -0300

    usando api sp

--- js/custom.js
+++ js/custom.js
@@ -30,49 +30,106 @@ const getEstados = () => {
   fetch(api).then(resposta => resposta.json()).then(json => {
     let options = '<option>Selecione</option>'
 
+    //console.log(json)
+    for (const index in json) {
+      //console.log(json[index].nome)
+      options += `<option value=${json[index].sigla}>${json[index].nome}</option>`
+    }
     select.innerHTML = options
   })
 
 }
 
+// Preencha o select de cidades de acordo com UF selecionado
+//A função recebe um parâmetro (uf) com o ID da UF
+const getCidadesByUf = (uf) => {
+  let api = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
+
+
+  let select = document.getElementById('cidade')
 
 
 
-/* -------------------------------------------*/
+
+  fetch(api).then(resposta => resposta.json()).then(json => {
+    let options = '<option>Selecione</option>'
 
 
-/*  //////////// EVENTOS E EXECUÇOES AUTOMATICAS ///////////////  */
+    for (const index in json) {
 
-getEstados()
+      options += `<option value=${json[index].nome}>${json[index].nome}</option>`
 
-mostraIdade()
-document.getElementById('idade').addEventListener('change', mostraIdade)
+    }
+    select.innerHTML = options
+  })
 
-mostraData()
+}
 
-// Inicializa animações scroll do AOS
-AOS.init();
+    /*  EXEMPLO LAÇO FOR
+    var semestre = ['jan', 'Fev', 'Marc', 'Abr', 'Mai', 'jun']
+    var texto = ''
+    for (let index = 0; index < semestre.length; index++) {
+      const element = semestre[index];
+      texto = element + '<br>'
+      document.getElementById('explorar').innerHTML += texto 
+    } */
 
 
-// Impede o envio do formulário quando os campos estão invalidos
 
-(function () {
-  'use strict'
 
-  // Captura as tags <form> que contem as classes "needs-validation"
-  var forms = document.querySelectorAll('.needs-validation')
 
-  // Executa para cada formulário da variável forms
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        //Se hover campos inválidos,interrompe o SUBMIT
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
 
-        form.classList.add('was-validated')
-      }, false)
+
+
+
+
+
+
+
+
+    /* -------------------------------------------*/
+
+
+    /*  //////////// EVENTOS E EXECUÇOES AUTOMATICAS ///////////////  */
+
+    getEstados()
+
+    mostraIdade()
+    document.getElementById('idade').addEventListener('change', mostraIdade)
+
+    mostraData()
+
+    // Inicializa animações scroll do AOS
+    AOS.init();
+
+
+    // Impede o envio do formulário quando os campos estão invalidos
+
+    (function () {
+      'use strict'
+
+      // Captura as tags <form> que contem as classes "needs-validation"
+      var forms = document.querySelectorAll('.needs-validation')
+
+      // Executa para cada formulário da variável forms
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+            //Se hover campos inválidos,interrompe o SUBMIT
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+          }, false)
+        })
+    })()
+
+
+    document.getElementById('estado').addEventListener('change', function () {
+      getCidadesByUf(this.value)
     })
-})()
+
+// this representa o estado 
+
